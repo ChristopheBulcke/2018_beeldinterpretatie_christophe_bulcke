759acce78a19494c6ab78ae025266cde8f1f53b8
ChristopheBulcke <christophe.bulcke@hotmail.be>
2018-10-30 12:00:56 +0100

    sessie1 finished

--- sessie1/sessie1/main.cpp
+++ sessie1/sessie1/main.cpp
@@ -6,6 +6,8 @@
 using namespace std;
 using namespace cv;
 
+RNG rng(12345);
+
 int main(int argc, const char** argv)
 {
     ///Adding a little help option and command line parser input
@@ -26,7 +28,7 @@ int main(int argc, const char** argv)
     ///Empty string warning
     string image_bimodal(parser.get<string>("image_bimodal"));
     string image_color(parser.get<string>("image_color"));
-    string image_color_adapted(parser.get<string>("image_color"));
+    string image_color_adapted(parser.get<string>("image_color_adapted"));
     if(image_bimodal.empty()){
         parser.printMessage();
         cerr <<"Empty parameter for bimodal image!"<<endl;
@@ -68,10 +70,13 @@ int main(int argc, const char** argv)
     }
     else{
         imshow(image_bimodal+" original",bimodal);
+        waitKey(0);
         imshow(image_color+" original",color);
+        waitKey(0);
         imshow(image_color_adapted+" original",color_adapted);
         waitKey(0);
         cerr<<"images read correctly"<<endl;
+        cerr<<image_color <<" "<< image_color_adapted <<endl;
     }
 
     ///Split the color images in the 3 colorchannels and apply a filter for skin color
@@ -86,7 +91,7 @@ int main(int argc, const char** argv)
 
     //put mask on image
     Mat finaal = Mat::zeros(Size(color.rows,color.cols),CV_8UC3);
-    cerr << channels[0].size() << " " << channels[0].type() << " " << skinPixels_mask.size() << " " << skinPixels_mask.type() << endl;
+    //cerr << channels[0].size() << " " << channels[0].type() << " " << skinPixels_mask.size() << " " << skinPixels_mask.type() << endl;
     Mat pixels_blue = channels[0] & skinPixels_mask;
     Mat pixels_green = channels[1] & skinPixels_mask;
     Mat pixels_red = channels[2] & skinPixels_mask;
@@ -96,28 +101,90 @@ int main(int argc, const char** argv)
     ch.push_back(pixels_green);
     ch.push_back(pixels_red);
 
-    cerr<<"hier zijn we"<<endl;
-
     merge(ch, finaal);
 
     imshow("Skin mask on image",finaal);
     waitKey(0);
 
     ///OTSU thresholding
-    //rgb->grijswaarde
-    //threshold
-    //histogram equalisatie equalizeHist(src.clone(), , , )
-    //clahe
+    //RGB 2 GRAY
+    Mat bim_gray;
+    cvtColor(bimodal,bim_gray,CV_RGB2GRAY);
 
-//    Mat result_CLAHE;
-//    Ptr<CLAHE> clahe_pointer = createCLAHE();
-//    clahe_pointer->setTilesGridSize(Size(15,15));
-//    clahe_pointer->setClipLimit(1);
-//    clahe_pointer->apply(gray_ticket.clone(), result_CLAHE);
-//    imshow("CLAHE ticket grayscale", result_CLAHE); waitKey(0);
-//
-//    Mat mask;
-//    threshold(result_CLAHE, mask, 0, 255, THRESH_OTSU | THRESH_BINARY);
+    //OTSU threshold
+    Mat bim_bw;
+    threshold(bim_gray,bim_bw, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+    imshow("Bimodal OTSU threshold", bim_bw);
+    waitKey(0);
+
+    //Histogram equalization then OTSU threshold
+    Mat bim_eq;
+    equalizeHist(bim_gray.clone(),bim_eq);
+    imshow("Bimodal hist. eq.",bim_eq);
+    threshold(bim_eq,bim_bw, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+    imshow("Bimodal hist. eq. OTSU threshold", bim_bw);
+    waitKey(0);
+
+    //CLAHE then OTSU threshold
+    Mat result_CLAHE;
+    Ptr<CLAHE> clahe_pointer = createCLAHE();
+    clahe_pointer->setTilesGridSize(Size(15,15));
+    clahe_pointer->setClipLimit(1);
+    clahe_pointer->apply(bim_gray.clone(), result_CLAHE);
+    imshow("Bimodal CLAHE", result_CLAHE); waitKey(0);
+    Mat mask;
+    threshold(result_CLAHE, mask, 0, 255, THRESH_OTSU | THRESH_BINARY);
+    imshow("Bimodal CLAHE OTSU threshold", mask); waitKey(0);
+
+    ///Erode and dilate
+    split(color_adapted, channels); //channel[0]=blue, channel[1]=green, channel[2]=red
+    skinPixels_mask = (channels[2]>95) & (channels[1]>40) & (channels[0]>20) & ((max(channels[2],max(channels[1],channels[0])) - min(channels[2],min(channels[1],channels[0])))>15) & (abs(channels[2]-channels[1])>15) & (channels[2]>channels[1]) & (channels[2]>channels[0]);
+    skinPixels_mask = skinPixels_mask * 255;
+    imshow("Skin pixels", skinPixels_mask);
+    waitKey(0);
+
+    //erode and dilate mask
+    Mat skinPixels_mask_adapted;
+    Point anchor = Point(-1,-1);
+
+    erode(skinPixels_mask,skinPixels_mask_adapted,Mat(),anchor,1);
+    dilate(skinPixels_mask_adapted,skinPixels_mask_adapted,Mat(),anchor,5);
+
+    //Connect all the blobs
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+
+    findContours( skinPixels_mask_adapted, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+    vector<vector<Point> >hull( contours.size() );
+    for( size_t i = 0; i < contours.size(); i++ ){
+        convexHull( Mat(contours[i]), hull[i], false );
+    }
+    Mat connected_blobs = Mat::zeros( skinPixels_mask_adapted.size(), CV_8UC1 );
+    for( size_t i = 0; i< contours.size(); i++ ){
+       drawContours( connected_blobs, hull, (int)i, Scalar(255,255,255), -1, 8, vector<Vec4i>(), 0, Point() );
+    }
+
+    imshow("Skin mask with erosian and dilation",connected_blobs);
+    waitKey(0);
+
+    //put mask on image
+    pixels_blue = channels[0] & connected_blobs;
+    pixels_green = channels[1] & connected_blobs;
+    pixels_red = channels[2] & connected_blobs;
+
+    vector<Mat> ch_adapted;
+    ch_adapted.push_back(pixels_blue);
+    ch_adapted.push_back(pixels_green);
+    ch_adapted.push_back(pixels_red);
+
+    Mat finaal_adapted = Mat::zeros(Size(color_adapted.rows,color_adapted.cols),CV_8UC3);
+
+    merge(ch_adapted, finaal_adapted);
+
+    imshow("Skin mask on image wtih erosian and dilation",finaal_adapted);
+
+    waitKey(0);
 //
 //    findcontours RETR_EXTERNAL
 
