8aa2d64708c6c8f77ddd99b8bd0380a1fdc4dfb7
ChristopheBulcke <christophe.bulcke@hotmail.be>
2018-12-31 16:22:02 +0100

    transformation finished

--- WhiteboardScanner/main.cpp
+++ WhiteboardScanner/main.cpp
@@ -6,6 +6,39 @@
 using namespace std;
 using namespace cv;
 
+vector<Point> boardCorners;
+
+void CallBackFuncCorners(int event, int x, int y, int flags, void* userdata)
+{
+    if  ( event == EVENT_LBUTTONDOWN )
+    {
+    //add element
+        if(boardCorners.size() == 4)
+        {
+            cout << "You can only add 4 corners." << endl;
+        }
+        else{
+            cout << "Added corner (" << x << ", " << y << ")" << endl;
+            //push_back: add element
+            boardCorners.push_back(Point(x,y));
+        }
+    }
+    else if  ( event == EVENT_RBUTTONDOWN )
+    {
+    //delete previous corner
+        if(boardCorners.size() > 0)
+        {
+            cout << "Deleted last added corner. " << boardCorners.size()-1 << " entries left." << endl;
+            //pop_back: delete previous element
+            boardCorners.pop_back();
+        }
+        else
+        {
+            cout << "There are no corners left in the list." << endl;
+        }
+    }
+}
+
 int main(int argc, const char** argv)
 {
     ///Adding a little help option and command line parser input
@@ -38,14 +71,77 @@ int main(int argc, const char** argv)
         cerr<< img_name + " not found"<<endl;
         return -1;
     }
-    else{
-        imshow(img_name + " original",img);
-        waitKey(0);
-    }
 
     ///Define whiteboard area
+    //new window
+    string cornersWindow("Select the 4 corners of the whiteboard clockwise, starting top left.");
+    namedWindow(cornersWindow);
+
+//    putText(img,
+//            "Click on the 4 corners of the whiteboard, clockwise!.",
+//            Point(5,20), // Coordinates
+//            FONT_HERSHEY_COMPLEX_SMALL, // Font
+//            0.9, // Scale. 2.0 = 2x bigger
+//            Scalar(255,255,255), // BGR Color
+//            1, // Line Thickness (Optional)
+//            CV_AA);
+
+    //define mouse callback
+    setMouseCallback(cornersWindow, CallBackFuncCorners, NULL);
+    imshow(cornersWindow,img);
+    waitKey(0);
+
+    Mat mask = Mat::zeros(img.rows,img.cols,CV_8UC3);
+    Mat boundingRectangle = Mat::zeros(img.rows,img.cols,CV_8UC3);
+
+    fillConvexPoly( mask, boardCorners, Scalar(255,255,255), LINE_AA );
+
+    imshow("mask",mask);
+    waitKey(0);
+
+    Mat board = Mat::zeros(img.rows,img.cols,CV_8UC3);
+    multiply(img,mask/255,board);
+
+    imshow("board",board);
+    waitKey(0);
 
     ///Transform perspective
+
+    Mat M, transformed;
+    M = Mat::zeros(img.rows,img.cols,img.type());
+
+    Rect r1 = boundingRect(boardCorners);
+    vector<Point> boundingRectanglePoints;
+    boundingRectanglePoints.push_back(Point(r1.x , r1.y));    //top left
+    boundingRectanglePoints.push_back(Point(r1.x+r1.width , r1.y));    //top right
+    boundingRectanglePoints.push_back(Point(r1.x+r1.width , r1.y+r1.height));    //bottom right
+    boundingRectanglePoints.push_back(Point(r1.x , r1.y+r1.height));    //bottom left
+
+    Point2f temp1[4];
+    Point2f temp2[4];
+
+    temp1[0] = boardCorners.at(0);
+    temp1[1] = boardCorners.at(1);
+    temp1[2] = boardCorners.at(2);
+    temp1[3] = boardCorners.at(3);
+
+    temp2[0] = boundingRectanglePoints.at(0);
+    temp2[1] = boundingRectanglePoints.at(1);
+    temp2[2] = boundingRectanglePoints.at(2);
+    temp2[3] = boundingRectanglePoints.at(3);
+
+    M = getPerspectiveTransform(temp1,temp2);
+    warpPerspective(board,transformed,M,transformed.size());
+    imshow("transformed mask",transformed);
+    waitKey(0);
+
+    Mat transformedCropped = Mat::zeros(r1.height,r1.width,img.type());
+
+    Mat tmp = transformed(Rect(r1.x,r1.y,r1.width,r1.height));
+    tmp.copyTo(transformedCropped);
+    imshow("transformedCropped",transformedCropped);
+    waitKey(0);
+
 
     ///Convert to HSV to segment different colors
     Mat img_hsv;
